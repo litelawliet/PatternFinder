d9c4d9513d11539eb8f91219530ba77b624e61bb
Clément <keshcrrat@hotmail.com>
2019-04-09 17:31:57 -0400

    Auto stash before merge of "master" and "origin/master"
    Open Close Door script

--- Pattern_Finder/Assets/Scripts/OpenCloseDoor.cs
+++ Pattern_Finder/Assets/Scripts/OpenCloseDoor.cs
@@ -8,21 +8,20 @@ public class OpenCloseDoor : MonoBehaviour
     [SerializeField] private bool open;
     [SerializeField] private GameObject door;
     [SerializeField] private Color image;
-    [SerializeField] private Color imageOpen = new Color(225, 225, 225, 255);
-    [SerializeField] private Color imageClose = new Color(125,125,125,255);
+    [SerializeField] private Color imageOpen = Color.green;
+    [SerializeField] private Color imageClose = Color.black;
     void Start()
     {
         door = this.gameObject;
         image = door.GetComponent<Image>().color;
         open = (image == imageOpen) ? true : !(image == imageClose);
-        
     }
     
     void Update()
     {
-        
+        door.GetComponent<Image>().color = open ? imageOpen : imageClose;
     }
-    
+
     public void DoorOpen()
     {
         open = !open;
