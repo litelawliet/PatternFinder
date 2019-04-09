1493bae0e40e2c13f8a711ddb05910ee385c6d5f
Jessy <calosjessy@gmail.com>
2019-04-09 16:57:58 -0400

    Menu_Scene corrigé

---
+++ Pattern_Finder/Assets/Scripts/SwitchScreen.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScreen : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject start;
+    [SerializeField]
+    private GameObject levelSelect;
+    [SerializeField]
+    private GameObject options;
+
+    public void Start()
+    {
+        start.SetActive(true);
+        levelSelect.SetActive(false);
+        options.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1_Scene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        options.SetActive(true);
+        start.SetActive(false);
+    }
+
+    public void LevelSelect()
+    {
+        start.SetActive(false);
+        levelSelect.SetActive(true);
+    }
+
+    public void Back()
+    {
+        options.SetActive(false);
+        levelSelect.SetActive(false);
+        start.SetActive(true);
+    }
+}
