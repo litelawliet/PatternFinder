eecdcf76a4144d17557b9f8599d363957ceae258
Clément <keshcrrat@hotmail.com>
2019-04-09 14:02:55 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Pattern_Finder/Assets/Scripts/Rotate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private bool rotate;
+    private GameObject mirror;
+    void Start()
+    {
+        mirror = this.gameObject;
+
+        if (mirror.transform.rotation.z == 90)
+        {
+            rotate = true;
+        }
+        if (mirror.transform.rotation.z == 0)
+        {
+            rotate = false;
+        }
+    }
+
+    void Update()
+    {
+        if (rotate == true)
+        {
+            mirror.transform.eulerAngles = new Vector3(0, 0, 90);
+        }
+        if (rotate == false)
+        {
+            mirror.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+    }
+
+    public void ButtonActivate()
+    {
+        rotate = !rotate;
+    }
+}
