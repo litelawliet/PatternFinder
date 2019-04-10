72deb9210a1fe33c80c12f871ee68e095430e297
Clément <keshcrrat@hotmail.com>
2019-04-10 12:18:03 -0400

    Script Door
    Door make the door move, not a color script anymore, able to receive laser

---
+++ Pattern_Finder/Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private bool open;
+    [SerializeField] private GameObject door;
+    [SerializeField] private Vector3 DoorPos;
+    [SerializeField] private Vector3 DoorOpen;
+    [SerializeField] private Vector3 DoorClose;
+    [SerializeField] private int reculPorte = -100;
+
+    private void Start()
+    {
+        door = gameObject;
+        DoorPos = door.transform.position;
+        DoorOpen = new Vector3(door.transform.position.x, door.transform.position.y, door.transform.position.z);
+        DoorClose = new Vector3(door.transform.position.x, door.transform.position.y, reculPorte);
+        open = DoorPos == DoorOpen || !(DoorPos == DoorClose);
+    }
+
+    private void Update()
+    {
+       door.transform.position = open ? DoorOpen : DoorClose;
+       //image = transform.position = new Vector3(0, 0, 10);
+    }
+
+    public void DoorActivate()
+    {
+        open = !open;
+    }
+}
