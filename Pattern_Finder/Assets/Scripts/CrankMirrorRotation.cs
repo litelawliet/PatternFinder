a00bb797326c994c08d016d1926fbf312ac9ff15
Thibaut PONCHON <thibaut.ponchon@gmail.com>
2019-04-12 00:27:04 -0400

    rotatin mirror using crank added. CrankManager in prefab, just drag the mirror and the crank you want in it

---
+++ Pattern_Finder/Assets/Scripts/CrankMirrorRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrankMirrorRotation : MonoBehaviour
+{
+    [SerializeField] private GameObject mirrorToRotate = null;
+    [SerializeField] private GameObject crankAssociated = null;
+    [Range(0, 1)] [SerializeField] private float rotatingSpeed = 0.1f;
+    private Transform mirrorTransform = null;
+    private Crank m_crankAssociated = null;
+
+    private void Start()
+    {
+        if (mirrorToRotate != null)
+        {
+            mirrorTransform = mirrorToRotate.transform.GetChild(0).transform;
+        }
+
+        if (crankAssociated != null)
+        {
+            m_crankAssociated = crankAssociated.GetComponent<Crank>();
+        }
+    }
+
+    private void Update()
+    {
+        if (mirrorToRotate != null && m_crankAssociated != null)
+        {
+            var rotation = mirrorTransform.rotation;
+            mirrorTransform.rotation = Quaternion.Euler(rotation.x, rotation.y,
+                m_crankAssociated.ResultAngle * rotatingSpeed);
+        }
+    }
+}
